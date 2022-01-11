d9d95a278dcc41cab0f90473de4ed697c7b774eb
echevrier <edith.chevrier@scs.ch>
2022-01-11 11:05:51 +0100

    Add teeracle1 test to CI

---
+++ scripts/teeracle1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+
+# Runs Teeracle1 demo: Either set `CLIENT_DIR` env var directly or run script with:
+#
+# source ./init_env.sh && ./teeracle1.sh
+
+cd "$CLIENT_DIR" || exit
+
+LOG_1="${LOG_1:-$LOG_DIR/teeracle1_demo_teeracle.log}"
+
+echo "[teeracle1.sh] printing to logs:"
+echo "        $LOG_1"
+
+touch "$LOG_1"
+
+./demo_teeracle.sh -p 9944 -P 2000 -d 24 -i 12 2>&1 | tee "$LOG_1"
